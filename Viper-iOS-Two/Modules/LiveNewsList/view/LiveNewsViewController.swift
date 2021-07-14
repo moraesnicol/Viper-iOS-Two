c9cd86e22380c0480bb214caf39357aea7ea397b
Gabriel Nicol <moraesnicol@gmail.com>
2021-07-14 12:01:57 -0300

    Table view added

--- Viper-iOS-Two/Modules/LiveNewsList/view/LiveNewsViewController.swift
+++ Viper-iOS-Two/Modules/LiveNewsList/view/LiveNewsViewController.swift
@@ -11,23 +11,62 @@ class LiveNewsViewController: UIViewController {
 
 
     var presenter: LiveNewsViewToPresenterProtocol?
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        setUpTableView()
+    }
+    
+    private func setUpTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(nibName: "LiveNewsTableViewCell", bundle: .main), forCellReuseIdentifier: "LiveNewsTableViewCell")
     }
 
-
 }
+
+extension LiveNewsViewController: UITableViewDataSource {
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter?.getNewsListCount() ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LiveNewsTableViewCell", for: indexPath) as? LiveNewsListTableViewCell
+        let row = indexPath.row
+        let news = presenter?.getNews(index: row)
+        guard let title = news?.title, let author = news?.author, let description = news?.description else {
+            return cell ?? UITableViewCell()
+        }
+        cell?.setCell(title: title, author: author, description: description)
+        return cell ?? UITableViewCell()
+    }
+}
+
+extension LiveNewsViewController: UITableViewDelegate {
+    
+}
+
+
 
 extension LiveNewsViewController: LiveNewsPresenterToViewProtocol {
     
     func showNews() {
+        tableView.reloadData()
         
     }
     
     func showError() {
+        let alert = UIAlertController(title: "Alert", message: "Problem Fetching News", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
        
     }
     
